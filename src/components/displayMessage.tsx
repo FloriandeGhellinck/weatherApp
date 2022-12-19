d5ec612da11ec970d8e8113a520ecc09b5b1a274
Florian de Ghellinck <f.deghellinck@gmail.com>
2022-12-19 17:51:37 +0100

    fix: devtool

--- src/components/displayMessage.tsx
+++ src/components/displayMessage.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import BoxComponent from './box';
 
-const DisplayMessage: FC<{ message: string }> = (props) => {
+const DisplayMessage: FC<{ message: string | undefined }> = (props) => {
   return (
     <BoxComponent>
       <h1 className='font-poppins text-2xl '>{props.message}</h1>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 }
